1f5520c98d5b34678a17fb70673ce60d4131e62b
Stephane Busso <stephane.busso@gmail.com>
2020-05-15 11:04:45 +1200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# vue-tailwind-template
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app" class="bg-white overflow-hidden shadow rounded-lg">
+    <div class="px-4 py-5 sm:p-6">
+      <transition name="fade" mode="out-in">
+        <router-view></router-view>
+      </transition>
+    </div>
+  </div>
+  <!-- </form-wizard> -->
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
